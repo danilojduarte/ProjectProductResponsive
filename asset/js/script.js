8c38f699ea2649b687cdba257bf952b39f264bdd
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-05-29 02:49:39 -0300

    Function javascript concluida

--- asset/js/script.js
+++ asset/js/script.js
@@ -3,6 +3,19 @@ const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close');
 
+//Menu Show
+  if ( navToggle ) {
+    navToggle.addEventListener('click',() => {
+        navMenu.classList.add('show-menu')
+    });
+  };
+
+//Menu Hidden
+if(navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  });
+};
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
