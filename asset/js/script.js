196289e47def4e726dc328a146db031068ab878c
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-06-01 15:01:53 -0300

    Aplicando javascript no efeito swiper

--- asset/js/script.js
+++ asset/js/script.js
@@ -28,7 +28,27 @@ const linkAction = () => {
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== SWIPER CLOTHING ===============*/
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
