18a3db8761aeede50a7a6be6d21745ac82434039
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-06-04 15:14:57 -0300

    Header concluido, iniciando animation

--- asset/js/script.js
+++ asset/js/script.js
@@ -51,6 +51,13 @@ let swiperHome = new Swiper('.home__swiper', {
 });
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const bgHeader = () => {
+  const header = document.getElementById('header')
 
+  this.scrollY >= 50 ? header.classList.add('bg-header')
+                     : header.classList.remove('bg-header')
+}
+
+window.addEventListener('scroll', bgHeader)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
